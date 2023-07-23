7b37e553e67fdb336372f82bec34782098bd08f4
joaomarcosschuh <jmschuh86@gmail.com>
2023-07-23 08:56:40 -0300

    rolagem

--- lib/views/category/category_page.dart
+++ lib/views/category/category_page.dart
@@ -87,180 +87,180 @@ class _CategoryPageState extends State<CategoryPage> {
     }
   }
 
+  // Your other methods here...
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(  // Adicione esta linha
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                'lib/assets/simbolos/foguinho.png',
-                                width: 40,
-                                height: 40,
-                              ),
-                              SizedBox(width: 5),
-                              Text(
-                                '$streak',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Center(
-                            child: CircleAvatar(
-                              backgroundImage: NetworkImage(photoUrl.isNotEmpty
-                                  ? photoUrl
-                                  : 'lib/assets/profile_picture.png'),
-                              radius: 35,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 10.0), // Add padding here
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'lib/assets/simbolos/foguinho.png',
+                              width: 40,
+                              height: 40,
                             ),
+                            SizedBox(width: 5),
+                            Text(
+                              '$streak',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Center(
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(photoUrl.isNotEmpty
+                                ? photoUrl
+                                : 'lib/assets/profile_picture.png'),
+                            radius: 35,
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '$totalXP',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$totalXP',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
                               ),
-                              SizedBox(width: 10),
-                              Image.asset(
-                                'lib/assets/simbolos/xp.png',
-                                width: 50,
-                                height: 50,
-                              ),
-                            ],
-                          ),
+                            ),
+                            SizedBox(width: 10),
+                            Image.asset(
+                              'lib/assets/simbolos/xp.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50.0),
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset(0, 10),
-                      blurRadius: 10.0,
                     ),
                   ],
-                ),
-                child: TextField(
-                  controller: _searchController,
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    hintStyle: TextStyle(color: Colors.black),
-                    border: InputBorder.none,
-                    icon: Icon(Icons.search, color: Colors.black),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50.0),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 10),
+                    blurRadius: 10.0,
                   ),
+                ],
+              ),
+              child: TextField(
+                controller: _searchController,
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: TextStyle(color: Colors.black),
+                  border: InputBorder.none,
+                  icon: Icon(Icons.search, color: Colors.black),
                 ),
               ),
             ),
-            SizedBox(height: 30),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 35.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.0),
-                      topRight: Radius.circular(20.0),
-                    ),
+          ),
+          SizedBox(height: 30),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 35.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
                   ),
-                  child: FutureBuilder<Map<String, Set<String>>>(
-                    future: _getCategories.fetchCategoriesAndDecks(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Center(
-                          child: Text(
-                            'An error occurred',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        );
-                      } else {
-                        final categories = snapshot.data!.entries
-                            .where((e) => e.key.contains(_searchController.text))
-                            .toList();
-                        return ListView.builder(
-                          itemCount: categories.length,
-                          itemBuilder: (context, index) {
-                            final category = categories[index].key;
-                            final decks = categories[index].value.toList();
-                            return Container(
-                              margin: EdgeInsets.symmetric(vertical: 10.0),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10.0),
-                                color: Colors.black,
-                              ),
-                              child: Theme(
-                                data: ThemeData(
-                                  textTheme: TextTheme(
-                                    titleMedium: TextStyle(color: Colors.white),
-                                  ),
-                                  unselectedWidgetColor: Colors.white,
+                ),
+                child: FutureBuilder<Map<String, Set<String>>>(
+                  future: _getCategories.fetchCategoriesAndDecks(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text(
+                          'An error occurred',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    } else {
+                      final categories = snapshot.data!.entries
+                          .where((e) => e.key.contains(_searchController.text))
+                          .toList();
+                      return ListView.builder(
+                        itemCount: categories.length,
+                        itemBuilder: (context, index) {
+                          final category = categories[index].key;
+                          final decks = categories[index].value.toList();
+                          return Container(
+                            margin: EdgeInsets.symmetric(vertical: 10.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Colors.black,
+                            ),
+                            child: Theme(
+                              data: ThemeData(
+                                textTheme: TextTheme(
+                                  titleMedium: TextStyle(color: Colors.white),
                                 ),
-                                child: ExpansionTile(
-                                  leading: CircleAvatar(
-                                    radius: 30,
-                                    backgroundImage:
-                                    AssetImage('lib/assets/${category.toLowerCase()}.jpg'),
-                                  ),
-                                  title: Text(category),
-                                  children: decks.map((deck) {
-                                    return DeckCheckbox(deck: deck, initialValue: false);
-                                  }).toList(),
-                                ),
+                                unselectedWidgetColor: Colors.white,
                               ),
-                            );
-                          },
-                        );
-                      }
-                    },
-                  ),
+                              child: ExpansionTile(
+                                leading: CircleAvatar(
+                                  radius: 30,
+                                  backgroundImage:
+                                  AssetImage('lib/assets/${category.toLowerCase()}.jpg'),
+                                ),
+                                title: Text(category),
+                                children: decks.map((deck) {
+                                  return DeckCheckbox(deck: deck, initialValue: false);
+                                }).toList(),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: Consumer<SelectedDecks>(
         builder: (context, selectedDecks, _) {
